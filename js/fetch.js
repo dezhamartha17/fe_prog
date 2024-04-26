9aec3b7c3a1f34694c68012d44aa5c22e5898058
dezhamartha17 <dezhamartha17@gmail.com>
2024-04-26 15:20:22 +0700

    remove /

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "http://127.0.0.1:8080/presensi/";
+let urlAPI = "http://127.0.0.1:8080/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);

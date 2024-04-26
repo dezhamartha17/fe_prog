f695b38db2dc7ebe8735ecdf7fe9235ef8cdfdfc
dezhamartha17 <dezhamartha17@gmail.com>
2024-04-26 15:16:05 +0700

    add pages

---
+++ js/fetch.js
@@ -0,0 +1,6 @@
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "http://127.0.0.1:8080/presensi/";
+get(urlAPI,isiTablePresensi);
+function isiTablePresensi(results){
+    console.log(results);
+}

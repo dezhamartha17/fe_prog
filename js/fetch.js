a666a0fb81c9c200f8a2c9034fdf934a363ee9c8
dezhamartha17 <dezhamartha17@gmail.com>
2024-04-26 15:30:26 +0700

    add table

--- js/fetch.js
+++ js/fetch.js
@@ -5,6 +5,15 @@ function isiTablePresensi(results){
     console.log(results);
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }

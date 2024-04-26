8e3d032ff66fbae52e5aead3518e7a12c83b3197
dezhamartha17 <dezhamartha17@gmail.com>
2024-04-26 15:24:39 +0700

    add isiRow

--- js/fetch.js
+++ js/fetch.js
@@ -3,4 +3,8 @@ let urlAPI = "http://127.0.0.1:8080/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
